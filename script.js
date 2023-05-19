ee0217f164936d7fd7a1c9674799bcf05eabe94f
mixeus <mixeuskaczala@gmail.com>
2023-05-19 18:01:12 -0300

    pop-up

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ let currentYear = new Date().getFullYear();
 let footerYear = document.getElementById("footer-year");
 footerYear.innerHTML = currentYear;
 
+//start pop-up//
+
+document.getElementById("openModalBtn").addEventListener("click", function() {
+  document.getElementById("myModal").style.display = "block";
+});
+
+document.getElementsByClassName("close")[0].addEventListener("click", function() {
+  document.getElementById("myModal").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+document.getElementById("openModalBtn").addEventListener("click", function() {
+  // Ação a ser executada quando o botão for clicado
+  console.log("Botão clicado!");
+  // Adicione aqui o código para abrir o pop-up ou realizar qualquer outra ação desejada
+});
